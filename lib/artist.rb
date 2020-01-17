bcbf5138100d8ad5f5378d9cbcf9661bc5428a46
Mia <tamiapaschall@gmail.com>
2020-01-17 20:31:15 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,3 +1,24 @@
 class Artist 
   @@all = []
-  attr_accessor :$FILENAME
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name 
+  end
+  
+  def self.all
+    @@all 
+  end
+  
+  def songs 
+    Song.find_by_artist(self)
+  end
+  
+  def self.find_or_created_by_name(name)
+    self.find(name) ? self.find(name) : self.create(name)
+  end
+  
+  def self.find(name)
+    self.all.find {|artist| artist.name ==name }
+  end
+  
